cb4f8494d77b9b90a10da096b87e426288fd4d44
emreariso <eibrahimarisoy@gmail.com>
2020-04-14 12:00:45 +0300

    Solutions of python3 introduction subdomain.

---
+++ python3/introduction/02.arithmetic_operators.py
@@ -0,0 +1,6 @@
+# Problem: https://www.hackerrank.com/challenges/python-arithmetic-operators/
+# Score: 10
+
+
+a, b = int(input()), int(input())
+print((a+b), (a-b), (a*b), sep='\n')
---
+++ python3/introduction/05.loops.py
@@ -0,0 +1,6 @@
+# https://www.hackerrank.com/challenges/python-loops/problem
+# Score: 10
+
+
+for i in range(int(input())):
+    print(i**2)
