f268759a0aba11e0b3fc6940525acf5735bed125
eibrahimarisoy <eibrahimarisoy@gmail.com>
2020-05-28 17:44:47 +0300

    updated settings

--- bogazicisut/settings/settings_server.py
+++ bogazicisut/settings/settings_server.py
@@ -4,9 +4,9 @@ from .settings_base import BASE_DIR, ALLOWED_HOSTS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'bogazici_sut',
-        'USER': 'bogazici_sut',
-        'PASSWORD': '123456789',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '5433',
     }
